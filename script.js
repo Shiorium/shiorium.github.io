6ab0caecd969eadf4f6d03a994ec82888d875aa6
Shiorium <142076431+Shiorium@users.noreply.github.com>
2023-08-12 01:59:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ const button = document.querySelector(".the-button");
 
 button.addEventListener("mousedown", () => {
   button.style.transform = "scale(1.01)";
+
+  const url = "https://github.com/Shiorium/shiorium.github.io/tree-commit-info/main/sounds";
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 304)) {
+          console.log(xhr.JSON);
+      }
+  };
+
+  xhr.send();
 });
 
 button.addEventListener("mouseup", () => {
