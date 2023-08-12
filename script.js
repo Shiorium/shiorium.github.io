3fde8bf78bc5bd057f9d49726be82355a16b493e
Shiorium <142076431+Shiorium@users.noreply.github.com>
2023-08-12 02:59:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ let latest = JSON.parse(localStorage.latest || JSON.stringify([]));
 const random = (lower, upper) => Math.floor(Math.random() * (upper - lower)) + lower;
 const randomSoundClip = (list) => list[random(0, list.length)];
 
-const button = document.querySelector(".the-button");
+const button = document.querySelector('.the-button');
 
-
-button.addEventListener("mouseup", () => {
-  button.style.transform = "scale(1)";
+button.addEventListener('mouseup', () => {
+  button.style.transform = 'scale(1)';
 });
+
+const count = async () => {
+
+};
 
 (async () => {
   const filenames = await fetch('manifest.json')
@@ -19,8 +22,8 @@ button.addEventListener("mouseup", () => {
 
   console.log(filenames);
 
-  button.addEventListener("mousedown", async () => {
-    button.style.transform = "scale(1.01)";
+  button.addEventListener('mousedown', async () => {
+    button.style.transform = 'scale(1.01)';
     const sound = filenames[random(0, filenames.length - 1)];
     const audio = new Audio(`sounds/${sound}`);
     audio.play();
