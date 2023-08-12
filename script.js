6b8c32c87fdaa591dcb71f5109fbdc10858330db
Shiorium <142076431+Shiorium@users.noreply.github.com>
2023-08-12 01:40:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,12 @@ let latest = JSON.parse(localStorage.latest || JSON.stringify([]));
 const rand = (lower, upper) => Math.floor(Math.random() * (upper - lower)) + lower;
 
 // script.js
-const imageButton = document.querySelector(".image-button");
-const buttonImage = imageButton.querySelector(".button-image");
+const button = document.querySelector(".the-button");
 
 imageButton.addEventListener("mousedown", () => {
-    buttonImage.style.transform = "scale(1.1)";
-    imageButton.style.backgroundImage = "url('active-image.jpg')";
+  button.style.transform = "scale(1.1)";
 });
 
 imageButton.addEventListener("mouseup", () => {
-    buttonImage.style.transform = "scale(1)";
-    imageButton.style.backgroundImage = "url('normal-image.jpg')";
+  button.style.transform = "scale(1)";
 });
