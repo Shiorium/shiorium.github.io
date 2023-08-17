e3c9949982bb22fc340fea4aecb12154ca781003
Shiorium <142076431+Shiorium@users.noreply.github.com>
2023-08-17 08:07:27 -0700

    update

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+const countEndpoint = 'https://api.shiorium.com:3000/count';
+
 let sessionCount = 0;
 let totalCount = parseInt(localStorage.sessionCount || JSON.stringify(0));
 let highest = parseInt(localStorage.highest || JSON.stringify(0));
+let globalRecord = JSON.parse(localStorage.globalRecord || JSON.stringify([]));
 
 document.getElementById('sessionCount').innerText = sessionCount + '';
 document.getElementById('totalCount').innerText = totalCount + '';
 document.getElementById('highest').innerText = highest + '';
+document.getElementById('globalRecord').innerHTML = globalRecord.map((x) => `<li>${x}</li>`).join('');
 
 const random = (lower, upper) => Math.floor(Math.random() * (upper - lower)) + lower;
 const randomSoundClip = (list) => list[random(0, list.length)];
@@ -29,6 +33,22 @@ const count = async () => {
     localStorage.highest = localStorage.sessionCount;
     document.getElementById('highest').innerText = sessionCount;
   }
+
+  const globalCount = await fetch(countEndpoint, {
+    method: 'POST',
+    headers: {
+      'Origin': 'https://shiorium.github.io',
+      'Content-Type': 'application/json',
+    }
+  })
+    .then(response => response.json())
+    .catch(error => console.error('Error fetching count JSON:', error));
+
+  globalRecord.unshift(globalCount.count);
+  localStorage.globalRecord = JSON.stringify(globalRecord);
+  
+  document.getElementById('globalCount').innerText = globalCount.count;
+  document.getElementById('globalRecord').innerHTML = globalRecord.map((x) => `<li>${x}</li>`).join('');
 };
 
 (async () => {
@@ -44,4 +64,14 @@ const count = async () => {
 
     count();
   });
+
+  const globalCount = await fetch(countEndpoint, {
+    method: 'GET',
+    headers: {
+      'Origin': 'https://shiorium.github.io',
+    }
+  })
+    .then(response => response.json())
+    .catch(error => console.error('Error fetching count JSON:', error));
+  document.getElementById('globalCount').innerText = (globalCount.count || 0) + '';
 })();
