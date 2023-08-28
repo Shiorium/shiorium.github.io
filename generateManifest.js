09285dacd67b2d488ee2911349df7fe2eeb798ef
Shiorium <142076431+Shiorium@users.noreply.github.com>
2023-08-27 17:04:57 -0700

    Update generateManifest.js

--- generateManifest.js
+++ generateManifest.js
@@ -18,4 +18,27 @@ fs.readdir(directoryPath, (err, files) => {
     }
     console.log('Manifest file generated successfully.');
   });
+
+  const identifiers = files.filter((x) => x.endsWith('.Identifier'));
+
+  identifiers.forEach(file => {
+    const filePath = path.join(directoryPath, file);
+
+    // Check if the file has the target extension
+    if (path.extname(filePath) === targetExtension) {
+      filesToDelete++;
+
+      fs.unlink(filePath, err => {
+        if (err) {
+          console.error('Error deleting file:', err);
+        } else {
+          filesDeleted++;
+          // Check if all files have been deleted
+          if (filesDeleted === filesToDelete) {
+            console.log('All files deleted.');
+          }
+        }
+      });
+    }
+  });
 });
